e2ea357971d3a9de989ab71b1677b1c0cadf93b4
LokeshSinh <lokeshsingh.jk@gmail.com>
2026-07-14 23:23:22 +0530

    first commit

--- LR-main/src/components/Home/HeroBanner.jsx
+++ LR-main/src/components/Home/HeroBanner.jsx
@@ -3,18 +3,19 @@ import { useState } from "react";
 import styles from "./HeroBanner.module.css";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import homeBanner1 from '../../assets/Home/homeBanner1.png'
+import buildingImg from '../../assets/Home/buildImg.png'
 
 function HeroBanner() {
   const [current, setCurrent] = useState(0);
 
   // Auto Slider
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       setCurrent((prev) => (prev === 2 ? 0 : prev + 1));
-//     }, 5000);
+  //   useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       setCurrent((prev) => (prev === 2 ? 0 : prev + 1));
+  //     }, 5000);
 
-//     return () => clearInterval(timer);
-//   }, []);
+  //     return () => clearInterval(timer);
+  //   }, []);
 
   // Next Slide
   const nextSlide = () => {
@@ -38,42 +39,75 @@ function HeroBanner() {
     <section className={styles.hero}>
       {/* ================= Slide 1 ================= */}
 
-      <div
-        className={`${styles.slide} ${
-          current === 0 ? styles.active : ""
-        }`}
-      >
-        <img
-          src={homeBanner1}
-          alt="Hero 1"
-          className={styles.bgImage}
-        />
+      <img src={homeBanner1} alt="Hero" className={styles.bgImage} />
 
-        <div className={styles.overlay}></div>
+      {/* Dark Overlay */}
+      <div className={styles.overlay}></div>
 
-        <div className={styles.content}>
+      {/* Watermark Text */}
+      <div className={styles.watermark}>
+        <h1>PRE-ENGINEERED BUILD AND SOLAR</h1>
+      </div>
+
+      {/* Hero Content */}
+      <div className={styles.container}>
+
+        {/* Left Content */}
+        <div className={styles.leftContent}>
+
+
           <h1 className={styles.title}>
-            L&R GREEN INDIA PVT LTDP <br />RECISION FABRICATION 
+            L&R GREEN INDIA PVT LTD <br />PRECISION FABRICATION
           </h1>
 
           <p className={styles.description}>
-            Manufacturer of prefabricated structures, modular cabins,
-            warehouses and industrial infrastructure solutions with
-            superior engineering quality.
+            Manufacturer of prefabricated structures, mild steel
+            containers & prefabricated cabins and solar.
+            <br />
+            Established in 2015, New Delhi.
           </p>
 
-          <button className={styles.button}>
-            Explore More
+          <button className={styles.heroBtn}>
+            <span>All Category</span>
           </button>
+
         </div>
+
+        {/* Building Image */}
+        <div className={styles.centerImage}>
+          <img src={buildingImg} alt="Building" />
+        </div>
+
+        {/* Right Statistics */}
+        <div className={styles.statsCard}>
+          <div className={styles.statBox}>
+            <h2>500+</h2>
+            <p>Completed Projects</p>
+          </div>
+
+          <div className={styles.statBox}>
+            <h2>25+</h2>
+            <p>Years Of Experience</p>
+          </div>
+
+          <div className={styles.statBox}>
+            <h2>900+</h2>
+            <p>Happy Clients</p>
+          </div>
+
+          <div className={styles.statBox}>
+            <h2>150+</h2>
+            <p>Team Workers</p>
+          </div>
+        </div>
+
       </div>
 
       {/* ================= Slide 2 ================= */}
 
       <div
-        className={`${styles.slide} ${
-          current === 1 ? styles.active : ""
-        }`}
+        className={`${styles.slide} ${current === 1 ? styles.active : ""
+          }`}
       >
         <img
           src="/Images/hero2.jpg"
@@ -109,9 +143,8 @@ function HeroBanner() {
       {/* ================= Slide 3 ================= */}
 
       <div
-        className={`${styles.slide} ${
-          current === 2 ? styles.active : ""
-        }`}
+        className={`${styles.slide} ${current === 2 ? styles.active : ""
+          }`}
       >
         <img
           src="/Images/hero3.jpg"
