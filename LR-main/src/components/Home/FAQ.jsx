a4c1c6cc7b29d70088ef40dd767f99032fa6b543
LokeshSinh <lokeshsingh.jk@gmail.com>
2026-07-14 17:26:46 +0530

    first commit

--- LR-main/src/components/Home/FAQ.jsx
+++ LR-main/src/components/Home/FAQ.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import styles from "../../styles/FAQ.module.css";
+import { Plus, X } from "lucide-react";
+import faq from '../../assets/Home/faq.png'
 
 const FAQ = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+
   const faqs = [
     {
       q: "What are Pre-Engineered Buildings (PEB)?",
@@ -25,42 +29,66 @@ const FAQ = () => {
     },
   ];
 
-  const [open, setOpen] = useState(1); // Second item open by default like in screenshot
+  const toggleFAQ = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
 
   return (
     <section className={styles["faq-section"]}>
-      <div className={styles["faq-left"]}>
+      <div className={styles.faqTitle}>
         <h2>
-          Frequently Asked <span>Questions</span>
+          Frequently Asked <br /><span>Questions</span>
         </h2>
-
-        <div className={styles["faq-list"]}>
-          {faqs.map((item, index) => (
-            <div
-              className={`${styles["faq-box"]} ${
-                open === index ? styles.open : ""
-              }`}
-              key={index}
-              onClick={() => setOpen(open === index ? null : index)}
-            >
-              <div className={styles["faq-question"]}>
-                <h4>{item.q}</h4>
-                <span>{open === index ? "−" : "+"}</span>
-              </div>
-
-              <div className={styles["faq-answer"]}>
-                <p>{item.a}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <div className={styles.FaqLine}></div>
       </div>
 
-      <div className={styles["faq-right"]}>
-        <img
-          src="https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=900&q=80"
-          alt="industrial steel building construction"
-        />
+      <div className={styles.FaqFlex}>
+        <div className={styles["faq-left"]}>
+
+
+          <div className={styles.container}>
+            {faqs.map((item, index) => (
+              <div
+                key={index}
+                className={`${styles.faqItem} ${activeIndex === index ? styles.active : ""
+                  }`}
+              >
+                <div
+                  className={styles.question}
+                  onClick={() => toggleFAQ(index)}
+                >
+                  <h3>{item.q}</h3>
+
+                  <span className={styles.icon}>
+                    {activeIndex === index ? (
+                      <X size={28} strokeWidth={2} />
+                    ) : (
+                      <Plus size={28} strokeWidth={2} />
+                    )}
+                  </span>
+                </div>
+
+                <div
+                  className={`${styles.answerWrapper} ${activeIndex === index ? styles.open : ""
+                    }`}
+                >
+                  <div className={styles.answer}>
+                    <p>{item.a}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* image */}
+
+        <div className={styles["faq-right"]}>
+          <img
+            src={faq}
+            alt="industrial steel building construction"
+          />
+        </div>
       </div>
     </section>
   );
